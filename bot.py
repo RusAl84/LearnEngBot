63d117f0b43d632ba60bb99e115d28fd73683629
Aleksey <rusal@bk.ru>
2022-02-16 02:04:22 +0300

    fix

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def get_text_messages(message):
     if str(message.text).lower() == "привет":
         bot.send_message(message.from_user.id, helpStr)
     elif str(message.text).lower() == "/help":
-        bot.send_message(message.from_user.id, "проект Physicsfights - Многофункциональный сервис для изучения физики \n Список команд: \n /s - вывести свой рейтинг \n /t - Получить задачу\n /a - ответ - дать ответ на задачу")
+        bot.send_message(message.from_user.id, "LearnEngBot - Многофункциональный сервис для изучения английского языка. \n Список команд: \n /s - вывести свой рейтинг \n /t - Получить задачу\n /a - ответ - дать ответ на задачу \n /h - Получить подсказку\n")
     elif splitted_text[0] == "/t":
         task, ans, taskid = getTask(message.from_user.id)
         bot.send_message(message.from_user.id, task)    
@@ -23,10 +23,16 @@ def get_text_messages(message):
         if CheckAns(message.from_user.id,taskid, splitted_text[1]):
             str1="Ответ верный"
         bot.send_message(message.from_user.id, str1)
+    elif splitted_text[0] == "/h":
+        # task, ans, taskid = getTask(message.from_user.id)
+        str1="Ответ не верный"
+        # if CheckAns(message.from_user.id,taskid, splitted_text[1]):
+        #     str1="Ответ верный"
+        # bot.send_message(message.from_user.id, str1)
     elif splitted_text[0] == "/s":
         score = getScore(message.from_user.id)
         bot.send_message(message.from_user.id, "Ваш рейтинг: "+ str(score))   
     else:
-        bot.send_message(message.from_user.id, "Для информации введите /help.")
+        bot.send_message(message.from_user.id, helpStr)
 
 bot.polling(none_stop=True, interval=0)
