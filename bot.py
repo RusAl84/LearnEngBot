dcc97d4a533c435c684ac92e8d2ac471c3764395
Aleksey <rusal@bk.ru>
2022-02-16 01:54:44 +0300

    fix

--- bot.py
+++ bot.py
@@ -8,9 +8,10 @@ bot = telebot.TeleBot('5111904045:AAEDPZLqKaacz7BFQV9Aohjj-5gmjEmoUCA');
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    helpStr="Привет, это проект LearnEngBot - Многофункциональный сервис для изучения английского языка. \n Для информации введите /help."
     splitted_text = str(message.text).lower().split()
     if str(message.text).lower() == "привет":
-        bot.send_message(message.from_user.id, "Привет, это проект Physicsfights - Многофункциональный сервис для изучения физики. \n Для информации введите /help.")
+        bot.send_message(message.from_user.id, helpStr)
     elif str(message.text).lower() == "/help":
         bot.send_message(message.from_user.id, "проект Physicsfights - Многофункциональный сервис для изучения физики \n Список команд: \n /s - вывести свой рейтинг \n /t - Получить задачу\n /a - ответ - дать ответ на задачу")
     elif splitted_text[0] == "/t":
